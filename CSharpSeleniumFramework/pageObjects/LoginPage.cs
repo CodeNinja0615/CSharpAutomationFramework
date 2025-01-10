6298e26f35dd7fe2e4e129a1cb8bfef81162f526
CodeNinja0615 <sameerakhtar1513@gmail.com>
2025-01-11 00:44:34 +0530

    Add project files.

---
+++ CSharpSeleniumFramework/pageObjects/LoginPage.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSeleniumFramework.pageObjects
+{
+    public class LoginPage
+    {
+        private IWebDriver driver;
+        public LoginPage(IWebDriver driver) 
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        //driver.FindElement(By.Id("username")).SendKeys("rahulshettyacademy");
+
+        //--Page object Factory
+        [FindsBy(How = How.Id, Using = "username")]
+        private IWebElement username;
+
+        [FindsBy(How = How.Name, Using = "password")]
+        private IWebElement password;
+
+        [FindsBy(How = How.XPath, Using = "//div[@class='form-group'][5]/label/span/input")]
+        private IWebElement checkBox;
+
+        [FindsBy(How = How.XPath, Using = "//input[@value='Sign In']")]
+        private IWebElement signIn;
+
+        public void Login()
+        {
+
+        }
+    }
+}
---
+++ CSharpSeleniumFramework/utilities/Base.cs
@@ -0,0 +1,63 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace CSharpSeleniumFramework.utilities
+{
+    public class Base
+    {
+        public IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            String browserName = ConfigurationManager.AppSettings["browser"];
+            if (browserName == null)
+            {
+                InitBrowser("Edge");
+            }
+            else
+            {
+                InitBrowser(browserName);
+            }
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
+        }
+
+        public IWebDriver getDriver()
+        {
+            return driver;
+        }
+        public void InitBrowser(string browserName) 
+        {
+            switch (browserName)
+            {
+                case "Firefox":
+                    driver = new FirefoxDriver();
+                    break;
+
+                case "Chrome":
+                    driver = new ChromeDriver();
+                    break;
+
+                case "Edge":
+                    driver = new EdgeDriver();
+                    break;
+            }
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Dispose();
+        }
+    }
+}
