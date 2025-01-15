5b20434ec57cc7148cb5f52e42f6e11d5615aa5d
CodeNinja0615 <sameerakhtar1513@gmail.com>
2025-01-16 03:37:41 +0530

    Integrated ExtentReport.
    Framework finalized

--- CSharpSeleniumFramework/utilities/Base.cs
+++ CSharpSeleniumFramework/utilities/Base.cs
@@ -9,18 +9,38 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
 using CSharpSeleniumFramework.testData;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports;
+using NUnit.Framework.Interfaces;
+using AventStack.ExtentReports.Model;
 
 namespace CSharpSeleniumFramework.utilities
 {
     public class Base
     {
         String browserName;
+        public ExtentReports extent;
+        public ExtentTest test;
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            String reportPath = projectDirectory + "//index.html";
+            var htmpRporter = new ExtentSparkReporter(reportPath);
+            extent = new ExtentReports();
+            extent.AttachReporter(htmpRporter);
+            extent.AddSystemInfo("Host Name", "Local host");
+            extent.AddSystemInfo("Environment", "QA");
+            extent.AddSystemInfo("UserName", "Sameer Akhtar");
+        }
         //public IWebDriver driver;
         public ThreadLocal<IWebDriver> driver = new();// ThreadLocal<IWebDriver>();
 
         [SetUp]
         public void StartBrowser()
         {
+            test =extent.CreateTest(TestContext.CurrentContext.Test.Name);
             browserName = TestContext.Parameters["browserName"];
             if (browserName == null)
             {
@@ -61,7 +81,33 @@ namespace CSharpSeleniumFramework.utilities
         [TearDown]
         public void CloseBrowser()
         {
-            driver.Value.Dispose();
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            var stacktrace = TestContext.CurrentContext.Result.StackTrace;
+
+            DateTime time = DateTime.Now;
+            String fileName = "Screenshot_" + time.ToString("hh_mm_ss") + ".png";
+            if(status == TestStatus.Failed)
+            {
+                test.Fail("Test Failed", CaptureScreenShot(driver.Value, fileName));
+                test.Log(Status.Fail, $"Test Failed with logtrace {stacktrace}");
+            }
+            else if(status == TestStatus.Passed)
+            {
+
+            }
+            driver.Value.Quit();
+            //driver.Value.Dispose();
+        }
+        [OneTimeTearDown]
+        public void tearDown(){
+            extent.Flush();
+        }
+        public Media CaptureScreenShot(IWebDriver driver, String screenshotName)
+        {
+            ITakesScreenshot ts = (ITakesScreenshot)driver;
+            var screenshot = ts.GetScreenshot().AsBase64EncodedString;
+            Media media = MediaEntityBuilder.CreateScreenCaptureFromBase64String(screenshot, screenshotName).Build();
+            return media;
         }
     }
 }
